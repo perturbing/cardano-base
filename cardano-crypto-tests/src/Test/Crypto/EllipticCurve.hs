42fc5b363ea0b07ace98e41b9f2fe084de8ea475
Thomas Vellekoop <107037423+perturbing@users.noreply.github.com>
2025-01-23 14:31:47 +0100

    Update cardano-crypto-tests/src/Test/Crypto/EllipticCurve.hs
    Co-authored-by: Alexey Kuleshevich <lehins@yandex.ru>

--- cardano-crypto-tests/src/Test/Crypto/EllipticCurve.hs
+++ cardano-crypto-tests/src/Test/Crypto/EllipticCurve.hs
@@ -138,8 +138,7 @@ testBLSCurve name _ =
         BLS.blsMult (BLS.blsMult a b) c === BLS.blsMult (BLS.blsMult a c) b
     , testProperty "scalar mult distributive left" $ \(a :: BLS.Point curve) (BigInteger b) (BigInteger c) ->
         BLS.blsMult a (b + c) === BLS.blsAddOrDouble (BLS.blsMult a b) (BLS.blsMult a c)
-    , testProperty "MSM matches naive approach" $ \(psAndSs :: [(BLS.Point curve, BigInteger)]) ->
-        not (null psAndSs) ==>
+    , testProperty "MSM matches naive approach" $ \(NonEmpty (psAndSs :: [(BLS.Point curve, BigInteger)])) ->
         let points = map fst psAndSs
             scalars = map ((\(BigInteger x) -> x) . snd) psAndSs
             pairs = NonEmpty.fromList (zip points scalars)
